80bcafc2ebdf236aa0b62f8283718418c336ea92
Ehomba <Milesbenovic@gmail.com>
2018-08-31 21:41:08 -0500

    hero

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var friends = require("../data/friends.js")
+var app = express();
+
+var router = new express.Router();
+
+
+router.get("/api/friends", function(req, res) {
+    
+
+    console.log(friends);
+  
+    // for (var i = 0; i < friends.length; i++) {
+        return res.json(friends);
+    }
+    // return res.json(false);
+  );
+
+router.post("/api/friends", function(req, res){
+    var score = [];
+    for(i = 0; i < 10; i++){
+    score.push(parseFloat(req.body.scores[i]))
+    }
+    for(k = 0; k < friends.length; k++){
+    for(i = 0; i < 10; i++){
+        var totalArray = [];
+        Math.abs(score[i] - friends[k].scores[i])
+    }
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var htmlRoute = require("./app/routing/htmlRoutes.js")
+var apiRoute = require("./app/routing/apiRoutes.js")
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(htmlRoute);
+app.use(apiRoute);
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
